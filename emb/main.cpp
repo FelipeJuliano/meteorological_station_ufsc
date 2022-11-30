34b769781d7cf74a9af0a15f6896fefc28e4e879
Felipe C. Juliano <46896206+FelipeJuliano@users.noreply.github.com>
2022-11-30 14:18:41 -0300

    add communications

--- emb/main.cpp
+++ emb/main.cpp
@@ -1,21 +1,33 @@
- */
+ 
 #include <stdexcept>
 #include <iostream>
 #include <chrono>
 #include <thread>
-
 #include "CRaspberryPi.hpp"
-#include "modules/CDht11.hpp"
+#include "CDht11.hpp"
+#include <CppLinuxSerial/SerialPort.hpp>
+
+using namespace mn::CppLinuxSerial;
 
 class dados
 {
 private:
  int temp;
- float umid, vel;
-
+ float umid;
+ const float pi = 3.14159265;     //Número de pi
+ int period = 5000;               //Tempo de medida(miliseconds)
+ int delaytime = 2000;            //Invervalo entre as amostras (miliseconds)
+ int radius = 147;                //Raio do anemometro(mm)
+ unsigned int Sample  = 0;        //Armazena o número de amostras
+ unsigned int counter = 0;        //Contador para o sensor  
+ unsigned int RPM = 0;            //Rotações por minuto
+ float speedwind = 0;             //Velocidade do vento (m/s)
+ float windspeed = 0;  
 public:
 
-   virtual void captadados();
+   virtual void anemometrof();
+   virtual void dht11();
+   friend void enviadados();
 
 };
 
@@ -29,7 +41,7 @@ void defineeventos();
 
 };
 
-void dados::captadados(){
+void dados::dht11(){
 
  try
     {
@@ -43,15 +55,16 @@ void dados::captadados(){
                 auto Data = Dht11.Measure();
                 temp = Data.Temperature;
                 umid = Data.Humidity;
-                std::cout << "Temperature: " << Data.Temperature << " °C" << std::endl;
-                std::cout << "Humidity: " << Data.Humidity << "%" << std::endl;
-                std::cout << std::endl;
+
             }
             catch (const std::exception& E)
             {
                 std::cout << "ERROR: " << E.what() << std::endl;
             }
         }
+
+
+        
     }
     catch (const std::exception& E)
     {
@@ -59,12 +72,60 @@ void dados::captadados(){
         return 1; 
 }
 
-
-
-
-
-
 ;}
+
+void dados::anemometrof()
+{
+try
+{
+  speedwind = 0;
+  windspeed = 0;
+  
+  counter = 0;  
+  attachInterrupt(0, addcount, RISING);
+  unsigned long millis();       
+  long startTime = millis();
+  while(millis() < startTime + period) {}
+
+  RPM =((counter)*60)/(period/1000);  // Calculate revolutions per minute (RPM)
+
+  windspeed = ((4 * pi * radius * RPM)/60) / 1000;  //Calcula a velocidade do vento em m/s
+
+  speedwind = (((4 * pi * radius * RPM)/60) / 1000)*3.6;  //Calcula velocidade do vento em km/h
+
+  counter++;
+}
+ catch (const std::exception& E)
+    {
+        std::cout << "ERROR: " << E.what() << std::endl;
+        return 1; 
+}
+
+};
+
+class enviadados
+{
+
+            SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_9600, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
+	// Use SerialPort serialPort("/dev/ttyACM0", 13000); instead if you want to provide a custom baud rate
+	serialPort.SetTimeout(-1); // Block when reading until any data is received
+	serialPort.Open();
+
+	// Read some data back (will block until at least 1 byte is received due to the SetTimeout(-1) call above)
+	std::string sendspeedwind;
+	serialPort.Send(speedwind);
+
+  std::string sendtemp;
+	serialPort.Send(readtemp);
+
+  std::string sendData;
+	serialPort.Send(readData);
+
+
+	serialPort.Close();
+};
+
+
 
 int main()
 {
